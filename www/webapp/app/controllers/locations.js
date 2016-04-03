7b0e5e47a7730442cf96d045440b4be11c07ce92
daixi <daixi859@gmail.com>
2016-04-03 22:06:36 +0800

    connect mongodb

--- www/webapp/app/controllers/locations.js
+++ www/webapp/app/controllers/locations.js
@@ -1,9 +1,34 @@
 exports.homelist = function(req, res) {
-    res.render('location-list', {title: 'Home'});
+    res.render('location-list', {
+        title: 'Home',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        locations: [{
+            name: 'Starcups',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 3,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '100m'
+        }, {
+            name: 'Cafe Hero',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 4,
+            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            distance: '200m'
+        }, {
+            name: 'Burger Queen',
+            address: '125 High Street, Reading, RG6 1PS',
+            rating: 2,
+            facilities: ['Food', 'Preminum wifi'],
+            distance: '250m'
+        }]
+    });
 };
 
 exports.locationInfo = function(req, res) {
-    res.render('location-info', {title: 'Location info'});
+    res.render('location-info', { title: 'Location info' });
 };
 
 exports.addReview = function(req, res) {
---
+++ www/webapp/app/models/db.js
@@ -0,0 +1,52 @@
+var mongoose = require('mongoose');
+
+/**
+ * mongodb://username:password@localhost:27027/database
+ * mongo ds041154.mlab.com:41154/angular-app -u <dbuser> -p <dbpassword>
+ */
+var dbURL = 'mongodb://localhost/Loc8r';
+if (process.env.NODE_ENV === 'production') {
+    dbURL = 'mongodb://dx859:85941049@23.83.231.244:20045/test';
+}
+mongoose.connect(dbURL);
+
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to ' + dbURL);
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+
+// Define function to accept message and callback function
+var gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() { // Close Mongoose connection, passing through an anonymous function to run when closed
+        console.log('Mongoose disconnected through ' + msg); // Output message and call callback when Mongoose connection is closed
+        callback();
+    });
+};
+
+// Listen for SIGUSR2, which is what nodemon uses
+process.once('SIGUSR2', function() {
+    // Send message to graceful- Shutdown and callback to kill process, emitting SIGUSR2 again
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app shutdown', function() {
+        process.exit(0);
+    });
+});
+
+
+require('./locations');
